bd37a21008eaa1d0f9ab4c85cc93e8ae65098936
lululemondagoat <folakeminounagnon@gmail.com>
2026-05-14 18:07:22 +0000

    fix: final deployment repairs

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,23 +1,22 @@
 import express, { type Express } from "express";
 import cors from "cors";
-import pinoHttp from "pino-http";
+import * as pinoHttp from "pino-http";
 import router from "./routes";
 import { logger } from "./lib/logger";
-
 const app: Express = express();
 
 app.use(
-  pinoHttp({
+  (pinoHttp as any).pinoHttp({
     logger,
     serializers: {
-      req(req) {
+      req(req: any) {
         return {
           id: req.id,
           method: req.method,
           url: req.url?.split("?")[0],
         };
       },
-      res(res) {
+      res(res: any) {
         return {
           statusCode: res.statusCode,
         };
@@ -25,6 +24,7 @@ app.use(
     },
   }),
 );
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
